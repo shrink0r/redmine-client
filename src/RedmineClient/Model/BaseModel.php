92f9f491676d136fbc51f93c81ba766ebe13bcc5
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-10-22 00:38:10 +0000

    finished implementing the ProjectTest.

--- src/RedmineClient/Model/BaseModel.php
+++ src/RedmineClient/Model/BaseModel.php
@@ -23,9 +23,17 @@ abstract class BaseModel
         foreach ($refClass->getProperties() as $property)
         {
             $propName = $property->getName();
+            $altName = strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $propName));
+
             if (isset($data[$propName]))
             {
+                // default camelcase fields
                 $this->$propName = $data[$propName];
+            }
+            elseif (isset($data[$altName]))
+            {
+                // also allow underscore fieldname spelling
+                $this->$propName = $data[$altName];
             }
         }
     }
--- test/src/RedmineClient/Model/ProjectTest.php
+++ test/src/RedmineClient/Model/ProjectTest.php
@@ -6,11 +6,53 @@ use RedmineClient\Model\Project;
 
 class ProjectTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCreate()
+    /**
+     * @dataProvider provideProjectData
+     */
+    public function testCreate(array $data)
     {
-        $project = Project::create(array(
-            // @todo provide fixture and assert the creation success against it.
-        ));
+        $project = Project::create($data);
+
         $this->assertInstanceOf('RedmineClient\Model\Project', $project);
+    }
+
+    /**
+     * @dataProvider provideProjectData
+     */
+    public function testGetters(array $data)
+    {
+        $project = Project::create($data);
+
+        $this->assertEquals($data['identifier'], $project->getIdentifier());
+        $this->assertEquals($data['description'], $project->getDescription());
+        $this->assertEquals($data['created_on'], $project->getCreatedOn());
+        $this->assertEquals($data['updated_on'], $project->getUpdatedOn());
+        $this->assertEquals($data['parent'], $project->getParent());
+        $this->assertEquals($data['name'], $project->getName());
+        $this->assertEquals($data['id'], $project->getId());
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public static function provideProjectData()
+    {
+        $data = array(
+            'identifier' => 'test-project',
+            'description' => 'Some text describing our test project.',
+            'created_on' => '2012/09/28 10:53:50 +0200',
+            'parent' => array(
+                'name' => 'parent-project',
+                'id' => 5,
+            ),
+            'updated_on' => '2012/09/28 10:53:50 +0200',
+            'name' => 'Test Project',
+            'id' => 23
+        );
+
+        $rows = array();
+        $rows[] = array($data);
+
+        return $rows;
     }
 }
